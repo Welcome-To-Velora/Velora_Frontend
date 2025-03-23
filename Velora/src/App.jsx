5ac9d420ab2bf9329a581a6832f49f8ba1480b75
Evan-Codes88 <Evan2401tm@gmail.com>
2025-03-23 15:15:59 +1000

    Feat: Initialised base pages

--- Velora/src/App.jsx
+++ Velora/src/App.jsx
@@ -1,6 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+
 function App() {
   return (
-    <div className="text-red-500 text-2xl">Hello World</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<LogIn />} />
+      </Routes>
+    </div>
   )
 }
 
