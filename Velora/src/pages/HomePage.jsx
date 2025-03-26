c5cc67c265794acb174ef6d511eceeb042129324
Evan-Codes88 <Evan2401tm@gmail.com>
2025-03-26 17:31:42 +1000

    Feat: Fixing home page

--- Velora/src/pages/HomePage.jsx
+++ Velora/src/pages/HomePage.jsx
@@ -4,16 +4,11 @@ import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>
+    <div className="app-container">
       <Navbar />
       <section className="hero-section">
         <div className="hero-content">
           <h1 className="hero-title">VELORA</h1>
-          <div className="circles-container">
-            <div className="circle circle-small"></div>
-            <div className="circle circle-medium"></div>
-            <div className="circle circle-large"></div>
-          </div>
         </div>
       </section>
     </div>
